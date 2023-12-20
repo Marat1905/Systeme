17662b6c5e43c7b764de2ba0e23e17893766c43e
Marat <milya010790@mail.ru>
2023-12-20 20:24:45 +0500

    Добавление библиотек и метода расширения для добавления сервисов моделей представления

---
+++ App/AppWpf/Services/ViewModelRegistrator.cs
@@ -0,0 +1,12 @@
+﻿using AppWpf.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppWpf.Services
+{
+    static class ViewModelRegistrator
+    {
+        public static IServiceCollection AddViewModels(this IServiceCollection services) => services
+            .AddSingleton<MainWindowViewModel>()
+            ;
+    }
+}
