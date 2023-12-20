b88199c3508e5abd093c256992ce32cf024bd226
Marat <milya010790@mail.ru>
2023-12-20 20:15:00 +0500

    Модель представление главного окна

---
+++ App/AppWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AppWpf.ViewModels.Base;
+
+namespace AppWpf.ViewModels
+{
+    internal class MainWindowViewModel: ViewModel
+    {
+        #region Заголовок окна. 
+        /// <summary>Заголовок окна.</summary>
+        private string _Title = "Главное окно";
+
+        /// <summary>Заголовок окна.</summary>
+        public string Title { get => _Title; set { Set(ref _Title, value); } }
+        #endregion
+
+    }
+}
