a99ecf1846009bc5bf17f97c435ed0b192f5c93b
Marat <milya010790@mail.ru>
2023-12-21 19:11:03 +0500

    Перечисление уровней логгирования

---
+++ Systeme.Logger/Enums/LogLevel.cs
@@ -0,0 +1,18 @@
+﻿namespace Systeme.Logger.Enums
+{
+    /// <summary>Перечисление уровней логгирования</summary>
+    public enum LogLevel
+    {
+        /// <summary>Отладочная информация</summary>
+        DebugInfo = 0,
+
+        /// <summary>Предупреждение</summary>
+        Warning = 1,
+
+        /// <summary>Ошибка</summary>
+        Error = 2,
+
+        /// <summary>Информация</summary>
+        Information = 3,
+    }
+}
