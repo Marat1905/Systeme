fea68a272f7f78b0acd4116b92bf13ea563de5f2
Marat <milya010790@mail.ru>
2023-12-21 19:18:10 +0500

    Интерфейс логгера

---
+++ Systeme.Logger/Interfaces/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using Systeme.Logger.Enums;
+
+namespace Systeme.Logger.Interfaces
+{
+    /// <summary>Интерфейс логгера </summary>
+    public interface ILogger
+    {
+        /// <summary>Метод для записи в лог</summary>
+        /// <param name="logLevel"><inheritdoc cref="LogLevel" path="/summary"/></param>
+        /// <param name="message">Сообщение</param>
+        void Write(LogLevel logLevel, string message);
+    }
+}
